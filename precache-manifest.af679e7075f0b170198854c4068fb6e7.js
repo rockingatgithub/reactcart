bb5d229255d8cfc17105216ba99063e1cdd626a6
rockingatgithub <contactwithsid@gmail.com>
2020-09-23 16:45:35 +0530

    Updates

---
+++ precache-manifest.af679e7075f0b170198854c4068fb6e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32beb24b0df7c8a8d666704f90280d92",
+    "url": "/reactcart/index.html"
+  },
+  {
+    "revision": "49e6fad9721de982d283",
+    "url": "/reactcart/static/css/main.968fd3d1.chunk.css"
+  },
+  {
+    "revision": "25a4190ffcdad7641b01",
+    "url": "/reactcart/static/js/2.fee3c5cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactcart/static/js/2.fee3c5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e6fad9721de982d283",
+    "url": "/reactcart/static/js/main.2c1a2d65.chunk.js"
+  },
+  {
+    "revision": "51e23c0099c3868bfef9",
+    "url": "/reactcart/static/js/runtime-main.76af9760.js"
+  }
+]);
